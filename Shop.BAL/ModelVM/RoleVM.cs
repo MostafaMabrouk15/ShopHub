302de37665f9d67417f4befc17f12c0169d46454
MostafaMabrouk-303 <mostafamabrouk4466@gmail.com>
2026-07-14 20:31:50 +0300

    Add Roles

---
+++ Shop.BAL/ModelVM/RoleVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop.BAL.ModelVM
+{
+    public class RoleVM
+    {
+
+        [Required(ErrorMessage = "Role is Required")]
+        [Display(Name = "Role Name")]
+        public string RoleName { get; set; }
+    }
+}
