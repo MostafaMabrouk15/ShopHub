d6066f301038bc1ca3edcffd1334092fd050df50
MostafaMabrouk-303 <mostafamabrouk4466@gmail.com>
2026-07-20 23:36:17 +0300

    Remove _dbContext From Controller&Add Include Props to GenericRepo

--- Shop.DAL/Repository/Abstraction/IGenericRepo.cs
+++ Shop.DAL/Repository/Abstraction/IGenericRepo.cs
@@ -4,8 +4,8 @@ namespace Shop.DAL.Repository.Abstraction
 {
     public interface IGenericRepo<T> where T : class
     {
-        IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll(string? IncludeProps = null);
+        T Get(Expression<Func<T, bool>> filter, string? IncludeProps = null);
         void Add(T item);
         void Remove(T item);
         void RemoveRange(IEnumerable<T> item);
